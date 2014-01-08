30424fea80dd4be9b78b26a740899b3c66aae066
Kostis at kilo <ankostis@gmail.com>
2014-01-08 10:36:33 +0100

    License under EUPL (latest) * Add LICENSE.txt file. * Add lic-header in all .vb files. * Add python script for adding lic-headers. * Add lic-header also in a powershell-script.

--- VECTO/File Browser/FB_Global.vb
+++ VECTO/File Browser/FB_Global.vb
@@ -1,4 +1,14 @@
-﻿Module FB_Global
+' Copyright 2014 European Union.
+' Licensed under the EUPL (the 'Licence');
+'
+' * You may not use this work except in compliance with the Licence.
+' * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+' * Unless required by applicable law or agreed to in writing,
+'   software distributed under the Licence is distributed on an "AS IS" basis,
+'   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+'
+' See the LICENSE.txt for the specific language governing permissions and limitations.
+Module FB_Global
 
     Public FB_FolderHistory(19) As String
     Public FB_Drives() As String
