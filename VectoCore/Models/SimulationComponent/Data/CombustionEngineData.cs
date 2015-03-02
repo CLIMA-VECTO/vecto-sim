1628e8cfc803c9dee1f0a8986009a8c19b40fe95
Markus Quaritsch <markus.quaritsch@tugraz.at>
2015-03-02 15:19:03 +0100

    adding test class, first models

--- VectoCore/Models/SimulationComponent/Data/CombustionEngineData.cs
+++ VectoCore/Models/SimulationComponent/Data/CombustionEngineData.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TUGraz.VectoCore.Models.Connector.Ports.Impl;
+using TUGraz.VectoCore.Models.SimulationComponent.Data.Engine;
 
 namespace TUGraz.VectoCore.Models.SimulationComponent.Data
 {
 	public class CombustionEngineData : SimulationComponentData
 	{
-		private InPort InPort{ get; set; }
+		/// <summary>
+		/// Engine description (e.g., mode, type, etc.
+		/// </summary>
+		public String ModelName { get; protected set; }
+		
+		/// <summary>
+		/// Engine displacement [ccm]
+		/// </summary>
+		public double Displacement { get; protected set; }
 
+		public double IdleSpeed { get; protected set; }
+
+		public double RatedSpeed { get; protected set; }
+
+		public double Inertia { get; protected set; }
+
+		public double MaxPower { get; set; }
+
+		public FuelConsumptionMap ConsumptionMap { get; protected set; }
+
+		public FullLoadCurve FullLoadCurve(uint gear)
+		{
+			throw new NotImplementedException("get FullLoadCurve for gear");
+		}
 	}
 }
---
+++ VectoCore/Models/SimulationComponent/Data/Engine/FullLoadCurve.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUGraz.VectoCore.Models.SimulationComponent.Data.Engine
+{
+	public class FullLoadCurve
+	{
+	}
+}
