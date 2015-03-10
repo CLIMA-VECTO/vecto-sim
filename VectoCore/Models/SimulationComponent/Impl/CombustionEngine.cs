4afc161e248988007f0032553c3ee3d4e4d5a011
Markus Quaritsch <markus.quaritsch@tugraz.at>
2015-03-10 15:58:23 +0100

    start working on engine model

--- VectoCore/Models/SimulationComponent/Impl/CombustionEngine.cs
+++ VectoCore/Models/SimulationComponent/Impl/CombustionEngine.cs
@@ -4,11 +4,32 @@ using TUGraz.VectoCore.Models.SimulationComponent.Data;
 
 namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 {
-    public class CombustionEngine : VectoSimulationComponent, ICombustionEngine, ITnOutPort
+
+	public class CombustionEngine : VectoSimulationComponent, ICombustionEngine, ITnOutPort
 	{
+		public enum EngineState
+		{
+			Idle,
+			Drag,
+			FullDrag,
+			Load,
+			FullLoad,
+			Stopped,
+			Undef
+		}
+
+		private CombustionEngineData _data;
+
+		private EngineState _state;
+		private double _currentEnginePower;
+		private double _currentEngineSpeed;
+
 		public CombustionEngine(CombustionEngineData data)
 		{
-
+			_data = data;
+			_state = EngineState.Idle;
+			_currentEnginePower = 0;
+			_currentEngineSpeed = _data.IdleSpeed;
 		}
 
 
@@ -20,13 +41,17 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 	    public override void CommitSimulationStep(IModalDataWriter writer)
 	    {
 	        writer[ModalResultField.FC] = 1;
-	        writer[ModalResultField.FC_AUXc] = 2;
-            writer[ModalResultField.FC_WHTCc] = 3;
+	        writer[ModalResultField.FCAUXc] = 2;
+            writer[ModalResultField.FCWHTCc] = 3;
 	    }
 
         public void Request(TimeSpan absTime, TimeSpan dt, double torque, double engineSpeed)
         {
+
             //throw new NotImplementedException();
         }
+
+		// accelleration los rotation engine
+		//Return (ENG.I_mot * (nU - nUBefore) * 0.01096 * ((nU + nUBefore) / 2)) * 0.001
 	}
 }
