7f7b082bdce75bdad79be42a400d25c107426de0
Michael Krisper <michael.krisper@tugraz.at>
2015-04-13 14:19:19 +0200

    added operators for si type conversions

--- VectoCore/Utils/Formulas.cs
+++ VectoCore/Utils/Formulas.cs
@@ -1,27 +1,27 @@
 namespace TUGraz.VectoCore.Utils
 {
-    public static class Formulas
-    {
-        /// <summary>
-        ///     [Nm], [rad/s] => [W]. Calculates the power from torque and angular velocity.
-        /// </summary>
-        /// <param name="torque">[Nm]</param>
-        /// <param name="angularFrequency">[rad/s]</param>
-        /// <returns>power [W]</returns>
-        public static Watt TorqueToPower(NewtonMeter torque, RadianPerSecond angularFrequency)
-        {
-            return (torque * angularFrequency).As<Watt>();
-        }
+	public static class Formulas
+	{
+		/// <summary>
+		///     [Nm], [rad/s] => [W]. Calculates the power from torque and angular velocity.
+		/// </summary>
+		/// <param name="torque">[Nm]</param>
+		/// <param name="angularFrequency">[rad/s]</param>
+		/// <returns>power [W]</returns>
+		public static Watt TorqueToPower(NewtonMeter torque, RadianPerSecond angularFrequency)
+		{
+			return torque * angularFrequency;
+		}
 
-        /// <summary>
-        ///     [W], [rad/s] => [Nm]. Calculates the torque from power and angular velocity.
-        /// </summary>
-        /// <param name="power">[W]</param>
-        /// <param name="angularFrequency">[rad/s]</param>
-        /// <returns>torque [Nm]</returns>
-        public static NewtonMeter PowerToTorque(Watt power, RadianPerSecond angularFrequency)
-        {
-            return (power / angularFrequency).As<NewtonMeter>();
-        }
-    }
+		/// <summary>
+		///     [W], [rad/s] => [Nm]. Calculates the torque from power and angular velocity.
+		/// </summary>
+		/// <param name="power">[W]</param>
+		/// <param name="angularFrequency">[rad/s]</param>
+		/// <returns>torque [Nm]</returns>
+		public static NewtonMeter PowerToTorque(Watt power, RadianPerSecond angularFrequency)
+		{
+			return power / angularFrequency;
+		}
+	}
 }
