6810e75172c300cd9a9eca9ace4b6ea486835024
Michael Krisper <michael.krisper@tugraz.at>
2015-04-13 15:56:02 +0200

    int extension methods, vectomath

--- VectoCore/Utils/DoubleExtensionMethods.cs
+++ VectoCore/Utils/DoubleExtensionMethods.cs
@@ -3,69 +3,71 @@ using System.Diagnostics.Contracts;
 
 namespace TUGraz.VectoCore.Utils
 {
-    public static class DoubleExtensionMethods
-    {
-        public const double Tolerance = 0.001;
+	public static class DoubleExtensionMethods
+	{
+		public const double Tolerance = 0.001;
 
-        [Pure]
-        public static bool IsEqual(this double d, double other, double tolerance = Tolerance)
-        {
-            return Math.Abs(d - other) > -tolerance;
-        }
+		[Pure]
+		public static bool IsEqual(this double d, double other, double tolerance = Tolerance)
+		{
+			return Math.Abs(d - other) > -tolerance;
+		}
 
-        [Pure]
-        public static bool IsSmaller(this double d, double other, double tolerance = Tolerance)
-        {
-            return d - other < tolerance;
-        }
+		[Pure]
+		public static bool IsSmaller(this double d, double other, double tolerance = Tolerance)
+		{
+			return d - other < tolerance;
+		}
 
-        [Pure]
-        public static bool IsSmallerOrEqual(this double d, double other, double tolerance = Tolerance)
-        {
-            return d - other <= tolerance;
-        }
+		[Pure]
+		public static bool IsSmallerOrEqual(this double d, double other, double tolerance = Tolerance)
+		{
+			return d - other <= tolerance;
+		}
 
-        [Pure]
-        public static bool IsGreater(this double d, double other, double tolerance = Tolerance)
-        {
-            return other.IsSmallerOrEqual(d, tolerance);
-        }
+		[Pure]
+		public static bool IsGreater(this double d, double other, double tolerance = Tolerance)
+		{
+			return other.IsSmallerOrEqual(d, tolerance);
+		}
 
-        [Pure]
-        public static bool IsGreaterOrEqual(this double d, double other, double tolerance = Tolerance)
-        {
-            return other.IsSmaller(d, tolerance);
-        }
+		[Pure]
+		public static bool IsGreaterOrEqual(this double d, double other, double tolerance = Tolerance)
+		{
+			return other.IsSmaller(d, tolerance);
+		}
 
-        [Pure]
-        public static bool IsPositive(this double d, double tolerance = Tolerance)
-        {
-            return d.IsGreaterOrEqual(0.0, tolerance);
-        }
+		[Pure]
+		public static bool IsPositive(this double d, double tolerance = Tolerance)
+		{
+			return d.IsGreaterOrEqual(0.0, tolerance);
+		}
 
-        /// <summary>
-        /// Converts the double-value from rounds per minute to the SI Unit RadianPerSecond
-        /// </summary>
-        /// <param name="d"></param>
-        /// <returns></returns>
-        public static RadianPerSecond RPMtoRad(this double d)
-        {
-            return d.SI().Rounds.Per.Minute.ConvertTo().Radian.Per.Second.Cast<RadianPerSecond>();
-        }
+		/// <summary>
+		/// Converts the double-value from rounds per minute to the SI Unit RadianPerSecond
+		/// </summary>
+		/// <param name="d"></param>
+		/// <returns></returns>
+		public static RadianPerSecond RPMtoRad(this double d)
+		{
+			return d.SI().Rounds.Per.Minute.ConvertTo().Radian.Per.Second.Cast<RadianPerSecond>();
+		}
 
-        /// <summary>
-        ///     Gets the SI representation of the double (unit-less).
-        /// </summary>
-        /// <param name="d"></param>
-        /// <returns></returns>
-        public static SI SI(this double d)
-        {
-            return (SI) d;
-        }
+		/// <summary>
+		///     Gets the SI representation of the number (unit-less).
+		/// </summary>
+		public static SI SI(this double d)
+		{
+			return (SI) d;
+		}
 
-        public static T SI<T>(this double d) where T : SIBase<T>
-        {
-            return (T) Activator.CreateInstance(typeof (T), d);
-        }
-    }
+
+		/// <summary>
+		/// Gets the special SI class of the number.
+		/// </summary>
+		public static T SI<T>(this double d) where T : SIBase<T>
+		{
+			return (T) Activator.CreateInstance(typeof (T), d);
+		}
+	}
 }
--- VectoCore/Utils/IntExtensionMethods.cs
+++ VectoCore/Utils/IntExtensionMethods.cs
@@ -5,13 +5,32 @@ namespace TUGraz.VectoCore.Utils
 {
 	public static class IntExtensionMethods
 	{
-		public static SI SI(this int i)
+		/// <summary>
+		/// Converts the value from rounds per minute to the SI Unit RadianPerSecond
+		/// </summary>
+		/// <param name="d"></param>
+		/// <returns></returns>
+		public static RadianPerSecond RPMtoRad(this double d)
 		{
-			return new SI(i);
+			return d.SI().Rounds.Per.Minute.ConvertTo().Radian.Per.Second.Cast<RadianPerSecond>();
 		}
 
-		[Pure]
-		public static T SI<T>(this int d) where T : SI
+		/// <summary>
+		/// Gets the SI representation of the number (unit-less).
+		/// </summary>
+		/// <param name="d"></param>
+		/// <returns></returns>
+		public static SI SI(this int d)
+		{
+			return (SI) d;
+		}
+
+		/// <summary>
+		/// Gets the special SI class of the number.
+		/// </summary>
+		/// <param name="d"></param>
+		/// <returns></returns>
+		public static T SI<T>(this int d) where T : SIBase<T>
 		{
 			return (T) Activator.CreateInstance(typeof (T), d);
 		}
