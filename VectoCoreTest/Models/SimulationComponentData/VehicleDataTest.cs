6eb098121600daa771498dd07ec80969e580e801
Markus Quaritsch <markus.quaritsch@tugraz.at>
2015-04-29 17:24:19 +0200

    adding test for reading vehicle data

---
+++ VectoCoreTest/Models/SimulationComponentData/VehicleDataTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TUGraz.VectoCore.Models.SimulationComponent.Data;
+
+namespace TUGraz.VectoCore.Tests.Models.SimulationComponentData
+{
+	[TestClass]
+	public class VehicleDataTest
+	{
+		private const string VehicleDataFile = @"TestData\Components\24t Coach.vveh";
+
+		[TestMethod]
+		public void ReadVehicleFileTest()
+		{
+			var vehicleData = VehicleData.ReadFromFile(VehicleDataFile);
+
+			Assert.AreEqual(VehicleData.VehicleCategory.Coach, vehicleData.Category);
+		}
+	}
+}
