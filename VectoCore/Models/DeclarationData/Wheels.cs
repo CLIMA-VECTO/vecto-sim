adfac04043dc5c6c8c31b1b511fed4efed0d7fe8
Markus Quaritsch <markus.quaritsch@tugraz.at>
2015-05-04 16:37:02 +0200

    adding declaration data, refactoring serialization

---
+++ VectoCore/Models/DeclarationData/Wheels.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Security.AccessControl;
+using TUGraz.VectoCore.Utils;
+
+namespace TUGraz.VectoCore.Models.DeclarationData
+{
+	public class Wheels
+	{
+		private static Wheels _instance;
+
+		private const string ResourceId = "DeclarationWheels";
+
+		protected Wheels()
+		{
+			System.Reflection.Assembly myAssembly;
+			//myAssembly = this.GetType().Assembly;
+			myAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+			//string[] resources = myAssembly.GetManifestResourceNames();
+			//string list = "";
+
+			//foreach (string resource in resources)
+			//	{
+			//	list += resource + "\r\n";
+			//	}
+			//System.Resources.ResourceManager myManager = new System.Resources.ResourceManager("Resources.Wheels", myAssembly);
+
+			System.IO.Stream file =
+				myAssembly.GetManifestResourceStream("TUGraz.VectoCore.Properties.Resources.resources");
+
+			//var csvFile = VectoCSVFile.ReadStream(file);
+			var reader = new StreamReader(file);
+			while (!reader.EndOfStream) {
+				var line = reader.ReadLine();
+			}
+			//var test = myManager.GetObject(ResourceId);
+		}
+
+		public static Wheels Instance()
+		{
+			if (_instance == null) {
+				_instance = new Wheels();
+			}
+			return _instance;
+		}
+	}
+}
---
+++ VectoCoreTest/Models/DeclarationData/DeclarationDataTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TUGraz.VectoCore.Models.DeclarationData;
+
+namespace TUGraz.VectoCore.Tests.Models.DeclarationData
+{
+	[TestClass]
+	public class DeclarationDataTest
+	{
+		[TestMethod]
+		public void WheelDataTest()
+		{
+			var wheels = Wheels.Instance();
+
+		}
+	}
+}
