55417207080da9d01415307fb7b9adfecd79759d
Markus Quaritsch <markus.quaritsch@tugraz.at>
2015-05-05 11:30:18 +0200

    adding resources to dll

--- VectoCore/Models/DeclarationData/Wheels.cs
+++ VectoCore/Models/DeclarationData/Wheels.cs
@@ -15,24 +15,18 @@ namespace TUGraz.VectoCore.Models.DeclarationData
 			System.Reflection.Assembly myAssembly;
 			//myAssembly = this.GetType().Assembly;
 			myAssembly = System.Reflection.Assembly.GetExecutingAssembly();
-			//string[] resources = myAssembly.GetManifestResourceNames();
-			//string list = "";
+			string[] resources = myAssembly.GetManifestResourceNames();
+			string list = "";
 
-			//foreach (string resource in resources)
-			//	{
-			//	list += resource + "\r\n";
-			//	}
-			//System.Resources.ResourceManager myManager = new System.Resources.ResourceManager("Resources.Wheels", myAssembly);
+			foreach (string resource in resources) {
+				list += resource + "\r\n";
+			}
+			//System.Resources.ResourceManager myManager = new System.Resources.ResourceManager("TUGraz.VectoCore.Resources.Declaration.Wheels.csv", myAssembly);
 
 			System.IO.Stream file =
-				myAssembly.GetManifestResourceStream("TUGraz.VectoCore.Properties.Resources.resources");
+				myAssembly.GetManifestResourceStream("TUGraz.VectoCore.Resources.Declaration.Wheels.csv");
 
-			//var csvFile = VectoCSVFile.ReadStream(file);
-			var reader = new StreamReader(file);
-			while (!reader.EndOfStream) {
-				var line = reader.ReadLine();
-			}
-			//var test = myManager.GetObject(ResourceId);
+			var csvFile = VectoCSVFile.ReadStream(file);
 		}
 
 		public static Wheels Instance()
