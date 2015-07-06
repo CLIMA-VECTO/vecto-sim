933f260db2f2a8732e096efc9808977ff90c95d6
Michael Krisper <michael.krisper@tugraz.at>
2015-07-06 15:48:01 +0200

    added slice helper method for strings and modulo for integers

--- VectoCore/Utils/StringExtensionMethods.cs
+++ VectoCore/Utils/StringExtensionMethods.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Globalization;
 
 namespace TUGraz.VectoCore.Utils
@@ -8,5 +9,14 @@ namespace TUGraz.VectoCore.Utils
 		{
 			return double.Parse(self, CultureInfo.InvariantCulture);
 		}
+
+        public static string Slice(this string s, int from = 0, int to = int.MaxValue)
+        {
+            from = Math.Min(Math.Max(from, -s.Length), s.Length);
+            from = from < 0 ? from + s.Length : from;
+            to = Math.Min(Math.Max(to, -s.Length), s.Length);
+            to = to < 0 ? to + s.Length : to;
+            return s.Substring(from, Math.Max(to - from, 0));
+        }
 	}
 }
