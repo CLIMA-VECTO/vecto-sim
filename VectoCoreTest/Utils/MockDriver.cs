e3402ec870ae6b62e58bc0d229887f4322cc0a1f
Markus Quaritsch <markus.quaritsch@tugraz.at>
2015-07-10 11:17:14 +0200

    adding testcase for minimal powertrain

---
+++ VectoCoreTest/Utils/MockDriver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TUGraz.VectoCore.Models.Connector.Ports;
+using TUGraz.VectoCore.Models.Simulation;
+using TUGraz.VectoCore.Models.Simulation.Data;
+using TUGraz.VectoCore.Models.SimulationComponent;
+using TUGraz.VectoCore.Utils;
+
+namespace TUGraz.VectoCore.Tests.Utils
+{
+	public class MockDriver : VectoSimulationComponent, IDriver, IDrivingCycleDemandOutPort, IDriverDemandInPort
+	{
+		private IDriverDemandOutPort _next;
+
+		public MockDriver(IVehicleContainer container) : base(container) {}
+
+		public override void CommitSimulationStep(IModalDataWriter writer) {}
+
+
+		public IDrivingCycleDemandOutPort OutShaft()
+		{
+			return this;
+		}
+
+		public IDriverDemandInPort InShaft()
+		{
+			return this;
+		}
+
+		public IResponse Request(TimeSpan absTime, TimeSpan dt, MeterPerSecond velocity, Radian gradient)
+		{
+			var acc = 0.SI<MeterPerSquareSecond>();
+			return _next.Request(absTime, dt, acc, 0.SI<Radian>());
+		}
+
+		public void Connect(IDriverDemandOutPort other)
+		{
+			_next = other;
+		}
+	}
+}
