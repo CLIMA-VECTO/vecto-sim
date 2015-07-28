e0e546c3fe2adf8c42641cbafebd84f94d8bbf58
Markus Quaritsch <markus.quaritsch@tugraz.at>
2015-07-28 09:14:52 +0200

    new testcase for overloading engine refactoring test - move parts of code to separate methods

--- VectoCoreTest/Integration/SimulationRuns/MinimalPowertrain.cs
+++ VectoCoreTest/Integration/SimulationRuns/MinimalPowertrain.cs
@@ -31,31 +31,9 @@ namespace TUGraz.VectoCore.Tests.Integration.SimulationRuns
 			var engineData = EngineeringModeSimulationDataReader.CreateEngineDataFromFile(EngineFile);
 			var cycleData = DrivingCycleDataReader.ReadFromFileDistanceBased(CycleFile);
 
-			var vehicleData = new VehicleData() {
-				AxleConfiguration = AxleConfiguration.AxleConfig_4x2,
-				CrossSectionArea = 0.SI<SquareMeter>(),
-				CrossWindCorrectionMode = CrossWindCorrectionMode.NoCorrection,
-				CurbWeight = 1000.SI<Kilogram>(),
-				CurbWeigthExtra = 0.SI<Kilogram>(),
-				Loading = 0.SI<Kilogram>(),
-				DynamicTyreRadius = 0.56.SI<Meter>(),
-				Retarder = new RetarderData() { Type = RetarderData.RetarderType.None },
-				AxleData = new List<Axle>(),
-				SavedInDeclarationMode = false,
-			};
+			var vehicleData = CreateVehicleData(0.SI<Kilogram>());
 
-			var driverData = new DriverData() {
-				AccelerationCurve = AccelerationCurveData.ReadFromFile(AccelerationFile),
-				LookAheadCoasting = new DriverData.LACData() {
-					Enabled = false,
-				},
-				OverSpeedEcoRoll = new DriverData.OverSpeedEcoRollData() {
-					Mode = DriverData.DriverMode.Off
-				},
-				StartStop = new VectoRunData.StartStopData() {
-					Enabled = false,
-				}
-			};
+			var driverData = CreateDriverData();
 
 			var modalWriter = new ModalDataWriter("Coach_MinimalPowertrain.vmod", false); //new TestModalDataWriter();
 			var sumWriter = new TestSumWriter();
@@ -87,7 +65,7 @@ namespace TUGraz.VectoCore.Tests.Integration.SimulationRuns
 
 			gbx.CurrentGear = 1;
 			var ds = Constants.SimulationSettings.DriveOffDistance;
-			while (vehicleContainer.Distance().Value() < 2000) {
+			while (vehicleContainer.Distance().Value() < 200) {
 				response = cyclePort.Request(absTime, ds);
 
 				switch (response.ResponseType) {
@@ -112,6 +90,106 @@ namespace TUGraz.VectoCore.Tests.Integration.SimulationRuns
 			modalWriter.Finish();
 			//var run = new DistanceRun(vehicleContainer);
 			//run.Run();
+		}
+
+
+		[TestMethod]
+		public void TestWheelsAndEngineOverload()
+		{
+			var engineData = EngineeringModeSimulationDataReader.CreateEngineDataFromFile(EngineFile);
+			var cycleData = DrivingCycleDataReader.ReadFromFileDistanceBased(CycleFile);
+
+			var vehicleData = CreateVehicleData(50000.SI<Kilogram>());
+
+			var driverData = CreateDriverData();
+
+			var modalWriter = new ModalDataWriter("Coach_MinimalPowertrainOverload.vmod", false); //new TestModalDataWriter();
+			var sumWriter = new TestSumWriter();
+			var vehicleContainer = new VehicleContainer(modalWriter, sumWriter);
+
+			var cycle = new DistanceBasedDrivingCycle(vehicleContainer, cycleData);
+
+			dynamic tmp = AddComponent(cycle, new Driver(vehicleContainer, driverData));
+			tmp = AddComponent(tmp, new Vehicle(vehicleContainer, vehicleData));
+			tmp = AddComponent(tmp, new Wheels(vehicleContainer, vehicleData.DynamicTyreRadius));
+			tmp = AddComponent(tmp, new Clutch(vehicleContainer, engineData));
+			AddComponent(tmp, new CombustionEngine(vehicleContainer, engineData));
+
+			var gbx = new DummyGearbox(vehicleContainer);
+
+			var cyclePort = cycle.OutPort();
+
+			cyclePort.Initialize();
+
+			gbx.CurrentGear = 0;
+
+			var absTime = 0.SI<Second>();
+			var response = cyclePort.Request(absTime, 1.SI<Meter>());
+
+			Assert.IsInstanceOfType(response, typeof(ResponseSuccess));
+
+			vehicleContainer.CommitSimulationStep(absTime, response.SimulationInterval);
+			absTime += response.SimulationInterval;
+
+			gbx.CurrentGear = 1;
+			var ds = Constants.SimulationSettings.DriveOffDistance;
+			while (vehicleContainer.Distance().Value() < 200) {
+				response = cyclePort.Request(absTime, ds);
+
+				switch (response.ResponseType) {
+					case ResponseType.DrivingCycleDistanceExceeded:
+						var rsp = response as ResponseDrivingCycleDistanceExceeded;
+						ds = rsp.MaxDistance;
+						continue;
+				}
+				Assert.IsInstanceOfType(response, typeof(ResponseSuccess));
+
+
+				vehicleContainer.CommitSimulationStep(absTime, response.SimulationInterval);
+				absTime += response.SimulationInterval;
+
+				ds = vehicleContainer.VehicleSpeed().IsEqual(0)
+					? Constants.SimulationSettings.DriveOffDistance
+					: (Constants.SimulationSettings.TargetTimeInterval * vehicleContainer.VehicleSpeed()).Cast<Meter>();
+
+				modalWriter.Finish();
+			}
+
+			modalWriter.Finish();
+			//var run = new DistanceRun(vehicleContainer);
+			//run.Run();
+		}
+
+		private static VehicleData CreateVehicleData(Kilogram loading)
+		{
+			return new VehicleData() {
+				AxleConfiguration = AxleConfiguration.AxleConfig_4x2,
+				CrossSectionArea = 0.SI<SquareMeter>(),
+				CrossWindCorrectionMode = CrossWindCorrectionMode.NoCorrection,
+				CurbWeight = 1000.SI<Kilogram>(),
+				CurbWeigthExtra = 0.SI<Kilogram>(),
+				Loading = loading,
+				DynamicTyreRadius = 0.56.SI<Meter>(),
+				Retarder = new RetarderData() { Type = RetarderData.RetarderType.None },
+				AxleData = new List<Axle>(),
+				SavedInDeclarationMode = false,
+			};
+		}
+
+		private static DriverData CreateDriverData()
+		{
+			return new DriverData() {
+				AccelerationCurve = AccelerationCurveData.ReadFromFile(AccelerationFile),
+				LookAheadCoasting = new DriverData.LACData() {
+					Enabled = false,
+				},
+				OverSpeedEcoRoll = new DriverData.OverSpeedEcoRollData() {
+					Mode = VectoCore.Models.SimulationComponent.Data.DriverData.DriverMode.Off
+				},
+				StartStop = new VectoRunData.StartStopData() {
+					Enabled = false,
+				}
+			};
 		}
 
 
