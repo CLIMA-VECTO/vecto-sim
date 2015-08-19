a0965e6f3cd890352a65e5696883943af70fea97
Michael Krisper <michael.krisper@tugraz.at>
2015-08-19 15:58:23 +0200

    changed gear in modalresults from SI to uint

--- VectoCore/Models/Simulation/Data/ModalResult.cs
+++ VectoCore/Models/Simulation/Data/ModalResult.cs
@@ -190,7 +190,7 @@ namespace TUGraz.VectoCore.Models.Simulation.Data
 		///     [-]	 GearData. "0" = clutch opened / neutral. "0.5" = lock-up clutch is open (AT with torque converter only, see
 		///     Gearbox)
 		/// </summary>
-		[ModalResultField(typeof(SI), caption: "Gear [-]")] Gear,
+		[ModalResultField(typeof(uint), caption: "Gear [-]")] Gear,
 
 		/// <summary>
 		///     [kW]	Gearbox losses.
