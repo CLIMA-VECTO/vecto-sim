96f7c605652019f1da4572f50bb3acc662b2e34c
Michael Krisper <michael.krisper@tugraz.at>
2015-08-19 14:40:04 +0200

    code formatting in help

--- User Manual/output/html/include.js
+++ User Manual/output/html/include.js
@@ -1,24 +1,24 @@
-$(function(){
-	var hash = window.location.hash;
-	if (hash){
-      $(hash.replace(".", "\\.")).show();
-      $(hash.replace(".", "\\.")).parents().show();
-   	} else
-   		$("#user-manual").show();
+$(function () {
+    var hash = window.location.hash;
+    if (hash) {
+        $(hash.replace(".", "\\.")).show();
+        $(hash.replace(".", "\\.")).parents().show();
+    } else $("#user-manual").show();
 
-    $('a').click(function(){
-    	$("body > div:not(#TOC)").hide();
-    	$($(this).attr("href").replace(".", "\\.")).show();
-	    $($(this).attr("href").replace(".", "\\.")).parents().show();
+    
+    $('a').click(function() {
+        $("body > div:not(#TOC)").hide();
+        $($(this).attr("href").replace(".", "\\.")).show();
+        $($(this).attr("href").replace(".", "\\.")).parents().show();
     });
 
-	$("#TOC").resizable({
-    	handles:"e",
-    	resize: function(event, ui) {
-        	$("body > div:not(#TOC)").css("padding-left", ui.size.width);
-  		}
+    $("#TOC").resizable({
+        handles: "e",
+        resize: function(event, ui) {
+            $("body > div:not(#TOC)").css("padding-left", ui.size.width);
+        }
     });
-    $("#TOC").scroll(function(){
-		$(".ui-resizable-handle").css('top', $("#TOC").scrollTop());
+    $("#TOC").scroll(function() {
+        $(".ui-resizable-handle").css('top', $("#TOC").scrollTop());
     });
 });
