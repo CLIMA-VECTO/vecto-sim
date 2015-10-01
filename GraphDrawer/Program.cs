0275896d87a6fc20c685d22b7d87adbd4ac14215
Markus Quaritsch <markus.quaritsch@tugraz.at>
2015-10-01 17:25:51 +0200

    comandline utility to generate graphs

---
+++ GraphDrawer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TUGraz.VectoCore.Tests.Utils;
+
+namespace GraphDrawer
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var imgV3 = @"..\..\..\VectoCoreTest\bin\Debug\Coach_DriverStrategy_Drive_50_slope_dec-inc.vmod";
+			var imgv22 =
+				@"..\..\..\VectoCoreTest\TestData\Integration\DriverStrategy\Vecto2.2\Coach\24t Coach_Cycle_Drive_30_Dec_Increasing_Slope.vmod";
+
+			GraphWriter.Write(imgV3, imgv22);
+			//GraphWriter.Write(args[0], args[1]);
+		}
+	}
+}
