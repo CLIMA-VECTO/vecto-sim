4a12622767fb5add3f19e573c8714ffb22d0811e
Markus Quaritsch <markus.quaritsch@tugraz.at>
2015-10-27 09:26:53 +0100

    testcases to run all declaration cycles

--- VectoCoreTest/Integration/FullCycleDeclarationTest.cs
+++ VectoCoreTest/Integration/FullCycleDeclarationTest.cs
@@ -2,9 +2,13 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NLog;
 using TUGraz.VectoCore.Configuration;
+using TUGraz.VectoCore.FileIO.Reader;
 using TUGraz.VectoCore.FileIO.Reader.Impl;
 using TUGraz.VectoCore.Models.Simulation.Data;
 using TUGraz.VectoCore.Models.Simulation.Impl;
+using TUGraz.VectoCore.Models.SimulationComponent.Data;
+using TUGraz.VectoCore.Models.SimulationComponent.Impl;
+using TUGraz.VectoCore.Utils;
 
 namespace TUGraz.VectoCore.Tests.Integration
 {
@@ -12,6 +16,106 @@ namespace TUGraz.VectoCore.Tests.Integration
 	public class FullCycleDeclarationTest
 	{
 		public const string TruckDeclarationJob = @"TestData\Integration\DeclarationMode\40t Truck\40t_Long_Haul_Truck.vecto";
+
+
+		[TestMethod]
+		public void Truck40t_LongHaulCycle_RefLoad()
+		{
+			var cycle = SimpleDrivingCycles.ReadDeclarationCycle("LongHaul");
+			var run = Truck40tPowerTrain.CreateEngineeringRun(cycle, "Truck40t_LongHaulCycle_RefLoad.vmod",
+				7500.SI<Kilogram>(), 12900.SI<Kilogram>());
+
+			run.Run();
+			Assert.IsTrue(run.FinishedWithoutErrors);
+		}
+
+		[TestMethod]
+		public void Truck40t_RegionalDeliveryCycle_RefLoad()
+		{
+			var cycle = SimpleDrivingCycles.ReadDeclarationCycle("RegionalDelivery");
+			var run = Truck40tPowerTrain.CreateEngineeringRun(cycle, "Truck40t_RegionalDeliveryCycle_RefLoad.vmod",
+				7500.SI<Kilogram>(), 12900.SI<Kilogram>());
+
+			run.Run();
+			Assert.IsTrue(run.FinishedWithoutErrors);
+		}
+
+		[TestMethod]
+		public void Truck40t_UrbanDeliveryCycle_RefLoad()
+		{
+			var cycle = SimpleDrivingCycles.ReadDeclarationCycle("UrbanDelivery");
+			var run = Truck40tPowerTrain.CreateEngineeringRun(cycle, "Truck40t_UrbanDeliveryCycle_RefLoad.vmod",
+				7500.SI<Kilogram>(), 12900.SI<Kilogram>());
+
+			run.Run();
+			Assert.IsTrue(run.FinishedWithoutErrors);
+		}
+
+		[TestMethod]
+		public void Truck40t_MunicipalCycle_RefLoad()
+		{
+			var cycle = SimpleDrivingCycles.ReadDeclarationCycle("Municipal");
+			var run = Truck40tPowerTrain.CreateEngineeringRun(cycle, "Truck40t_MunicipalCycle_RefLoad.vmod",
+				7500.SI<Kilogram>(), 12900.SI<Kilogram>());
+
+			run.Run();
+			Assert.IsTrue(run.FinishedWithoutErrors);
+		}
+
+		[TestMethod]
+		public void Truck40t_ConstructionCycle_RefLoad()
+		{
+			var cycle = SimpleDrivingCycles.ReadDeclarationCycle("Construction");
+			var run = Truck40tPowerTrain.CreateEngineeringRun(cycle, "Truck40t_ConstructionCycle_RefLoad.vmod",
+				7500.SI<Kilogram>(), 12900.SI<Kilogram>());
+
+			run.Run();
+			Assert.IsTrue(run.FinishedWithoutErrors);
+		}
+
+		[TestMethod]
+		public void Truck40t_HeavyUrbanCycle_RefLoad()
+		{
+			var cycle = SimpleDrivingCycles.ReadDeclarationCycle("HeavyUrban");
+			var run = Truck40tPowerTrain.CreateEngineeringRun(cycle, "Truck40t_HeavyUrbanCycle_RefLoad.vmod",
+				7500.SI<Kilogram>(), 12900.SI<Kilogram>());
+
+			run.Run();
+			Assert.IsTrue(run.FinishedWithoutErrors);
+		}
+
+		[TestMethod]
+		public void Truck40t_SubUrbanCycle_RefLoad()
+		{
+			var cycle = SimpleDrivingCycles.ReadDeclarationCycle("SubUrban");
+			var run = Truck40tPowerTrain.CreateEngineeringRun(cycle, "Truck40t_SubUrbanCycle_RefLoad.vmod",
+				7500.SI<Kilogram>(), 12900.SI<Kilogram>());
+
+			run.Run();
+			Assert.IsTrue(run.FinishedWithoutErrors);
+		}
+
+		[TestMethod]
+		public void Truck40t_InterUrbanCycle_RefLoad()
+		{
+			var cycle = SimpleDrivingCycles.ReadDeclarationCycle("InterUrban");
+			var run = Truck40tPowerTrain.CreateEngineeringRun(cycle, "Truck40t_InterUrbanCycle_RefLoad.vmod",
+				7500.SI<Kilogram>(), 12900.SI<Kilogram>());
+
+			run.Run();
+			Assert.IsTrue(run.FinishedWithoutErrors);
+		}
+
+		[TestMethod]
+		public void Truck40t_CoachCycle_RefLoad()
+		{
+			var cycle = SimpleDrivingCycles.ReadDeclarationCycle("Coach");
+			var run = Truck40tPowerTrain.CreateEngineeringRun(cycle, "Truck40t_CoachCycle_RefLoad.vmod",
+				7500.SI<Kilogram>(), 12900.SI<Kilogram>());
+
+			run.Run();
+			Assert.IsTrue(run.FinishedWithoutErrors);
+		}
 
 		[TestMethod]
 		public void Truck40tDeclarationTest()
