6da567a54e7c727d0c90956de54d2f2258e03545
Michael Krisper <michael.krisper@tugraz.at>
2015-10-28 11:28:50 +0100

    changes for engineering and declaration mode, when files were saved in other mode

--- VectoConsole/Program.cs
+++ VectoConsole/Program.cs
@@ -83,7 +83,7 @@ Examples:
 				var jobContainer = new JobContainer(sumWriter);
 
 				foreach (var file in fileList.Where(f => Path.GetExtension(f) == Constants.FileExtensions.VectoJobFile)) {
-					var runsFactory = new SimulatorFactory(SimulatorFactory.FactoryMode.EngineeringMode, file);
+					var runsFactory = new SimulatorFactory(SimulatorFactory.FactoryMode.DeclarationMode, file);
 					jobContainer.AddRuns(runsFactory);
 				}
 
